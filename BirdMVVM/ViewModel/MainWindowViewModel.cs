70c3dbb917ffd2f822a9a274d3be514731f470a2
Jonathan Markman <jmarkman446@gmail.com>
2018-03-15 17:25:23 -0400

    got core concept working, added folder structure, comments

--- BirdMVVM/ViewModel/MainWindowViewModel.cs
+++ BirdMVVM/ViewModel/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 
